7ad7db12e038feb3bd507445fd4f942963c854a0
tegitting <159534945+tegitting@users.noreply.github.com>
2024-11-12 23:26:06 +0000

    Update __init__.py

--- pynicotine/plugins/leech_detector/__init__.py
+++ pynicotine/plugins/leech_detector/__init__.py
@@ -173,7 +173,7 @@ class Plugin(BasePlugin):
 
     def user_stats_notification(self, user, stats):
         self.log("[INFO] Source of stats is %s", stats["source"])
-        
+
         if "private_dirs" in stats:
             self.log("[INFO] %s stats where requested by the plugin", user)
             # self.check_user(user, num_files=stats["files"], num_folders=stats["dirs"], source=stats["source"])
