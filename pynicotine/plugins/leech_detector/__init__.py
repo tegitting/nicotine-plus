008b86855d5a8f87d83a17ab89846bedd0e8c7e8
tegitting <159534945+tegitting@users.noreply.github.com>
2024-11-13 01:00:42 +0000

    Update __init__.py

--- pynicotine/plugins/leech_detector/__init__.py
+++ pynicotine/plugins/leech_detector/__init__.py
@@ -34,6 +34,7 @@ class Plugin(BasePlugin):
         super().__init__(*args, **kwargs)
 
         self.settings = {
+            "enable_ban": False,
             "send_message": True,
             "open_private_chat": True,
             "message": "Please consider sharing more files if you would like to download from me again. Thanks :)",
@@ -96,7 +97,7 @@ class Plugin(BasePlugin):
             (self.settings["num_files"], self.settings["num_folders"])
         )
 
-    def check_user(self, user, num_files, num_folders, source="server"):
+    def check_user(self, user, files, folders, private_folders, locked_percent, total):
 
         if user not in self.probed_users:
             # We are not watching this user
@@ -106,23 +107,46 @@ class Plugin(BasePlugin):
             # User was already accepted previously, nothing to do
             return
 
-        if self.probed_users[user] == "requesting_shares" and source != "peer":
-            # Waiting for stats from peer, but received stats from server. Ignore.
-            return
-
-        is_user_accepted = (num_files >= self.settings["num_files"] and num_folders >= self.settings["num_folders"])
-
-        if is_user_accepted or user in self.core.buddies.users:
+        # conditions to avoid detection
+        is_user_validated = (
+            files >= self.settings["num_files"]
+            and folders >= self.settings["num_folders"]
+            and locked_percent < self.settings["percentage_threshold"]
+        )
+        # when the user meets criteria or is a buddy
+        if is_user_validated or user in self.core.buddies.users:
+            # check if they exist in the leechers list
             if user in self.settings["detected_leechers"]:
+                # and remove them
                 self.settings["detected_leechers"].remove(user)
 
+            # mark the user as OK
             self.probed_users[user] = "okay"
 
             if is_user_accepted:
-                self.log("User %s is okay, sharing %s files in %s folders.", (user, num_files, num_folders))
+                self.log(
+                    "[USER] %s is OK. Sharing %s files %s folders with %s private. %s percent of %s is locked",
+                    (
+                        user,
+                        files,
+                        folders,
+                        private_folders,
+                        locked_percent,
+                        human_size(total),
+                    ),
+                )
             else:
-                self.log("Buddy %s is sharing %s files in %s folders. Not complaining.",
-                         (user, num_files, num_folders))
+                self.log(
+                    "[BUDDY] %s has %s files in %s folders with %s private. %s percent of %s is locked.",
+                    (
+                        user,
+                        files,
+                        folders,
+                        private_folders,
+                        locked_percent,
+                        human_size(total),
+                    ),
+                )
             return
 
         if not self.probed_users[user].startswith("requesting"):
@@ -134,24 +158,46 @@ class Plugin(BasePlugin):
             self.probed_users[user] = "processed_leecher"
             return
 
-        if (num_files <= 0 or num_folders <= 0) and self.probed_users[user] != "requesting_shares":
-            # SoulseekQt only sends the number of shared files/folders to the server once on startup.
-            # Verify user's actual number of files/folders.
-            self.log("User %s has no shared files according to the server, requesting shares to verify…", user)
+        log_message = "[DETECTED LEECH] %s has %s files %s folders and %s private. %s percent of %s locked."
+        self.log(
+            log_message,
+            (user, files, folders, private_folders, locked_percent, human_size(total)),
+        )
 
-            self.probed_users[user] = "requesting_shares"
-            self.core.userbrowse.request_user_shares(user)
-            return
+        # no message configured
+        if not self.settings["message"]:
+            self.log(
+                "[DETECTED LEECH] %s doesn't share enough. No message specified in plugin.",
+                user,
+            )
 
-        if self.settings["message"]:
-            log_message = ("Leecher detected, %s is only sharing %s files in %s folders. Going to message "
-                           "leecher after transfer…")
+        # send the configured message
         else:
-            log_message = ("Leecher detected, %s is only sharing %s files in %s folders. Going to log "
-                           "leecher after transfer…")
+            for line in self.settings["message"].splitlines():
+                for placeholder, option_key in self.PLACEHOLDERS.items():
+                    # Replace message placeholders with actual values specified in the plugin settings
+                    line = line.replace(placeholder, str(self.settings[option_key]))
+                self.send_private(
+                    user,
+                    line,
+                    show_ui=self.settings["open_private_chat"],
+                    switch_page=False,
+                )
+            self.log(
+                "[DETECTED LEECH] %s doesn't share enough. A message was sent.", user
+            )
 
-        self.probed_users[user] = "pending_leecher"
-        self.log(log_message, (user, num_files, num_folders))
+        # add the user to the detected leecher list
+        if user not in self.settings["detected_leechers"]:
+            self.settings["detected_leechers"].append(user)
+
+        # if a ban is required
+        if self.settings["enable_ban"] is True:
+            self.core.networkfilter.ban_user(user)
+            self.log("[DETECTED LEECH] %s has been banned.", user)
+
+        # mark as processed
+        self.probed_users[user] = "processed_leecher"
 
     def upload_queued_notification(self, user, virtual_path, real_path):
 
@@ -159,7 +205,7 @@ class Plugin(BasePlugin):
         if user in self.probed_users:
             return
 
-        self.probed_users[user] = "requesting_stats"
+        self.probed_users[user] = "requesting_shares"
 
         if user not in self.core.users.watched:
             # Transfer manager will request the stats from the server shortly
@@ -172,14 +218,35 @@ class Plugin(BasePlugin):
         self.core.userbrowse.browse_user(user)
 
     def user_stats_notification(self, user, stats):
-        self.log("[INFO] Source of stats is %s", stats["source"])
-        # my_dict = stats
-        # key_1 = 'source'
-        # key_2 = 'private_dirs'
-        if stats.get('source') is not None and stats.get('private_dirs') is not None:
-            # if stats["source"] == "peer" and self.probed_users[user] == "requesting_shares":
-            self.log("[INFO] %s stats where requested by the plugin", user)
-            # self.check_user(user, num_files=stats["files"], num_folders=stats["dirs"], source=stats["source"])
+
+        # only process if private_dirs in stats and we have asked the user for shares
+        if stats.get('private_dirs') is not None and self.probed_users[user] == "requesting_shares":
+
+        # cleanup before processing
+        files = stats["files"]
+        folders = stats["dirs"]
+        private_folders = stats["private_dirs"]
+        share_total = stats["shared_size"]
+        # add up all the folders
+        total_folders = folders + private_folders
+        # calculate locked percentage
+        if private_folders:
+            locked_percent = round((private_folders / total_folders) * 100)
+        else:
+            locked_percent = 0
+    
+        # log our progress
+        self.log("[INFO] %s shares received, processing...", user)
+        
+        # invoke check user
+        self.check_user(
+            user,
+            files,
+            folders,
+            private_folders,
+            locked_percent,
+            share_total,
+        )
 
     def upload_finished_notification(self, user, *_):
 
